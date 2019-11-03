7f5b0e46fe118944d8debe2421f01135467413b8
mperdeck@gmail.com <mperdeck@gmail.com>
2019-11-03 17:29:01 +1100

    add super basic crud service IService

---
+++ attributeapi/Services/IService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Services
+{
+    public interface IService<T> where T : class
+    {
+        void Add(T data);
+        void Delete(Guid externalId);
+        T Get(Guid externalId);
+        void Update(T data);
+    }
+}
---
+++ attributeapi/Services/Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Services
+{
+    /// <summary>
+    /// Super basic CRUD service, direct to database
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Service<T> : IService<T> where T : class
+    {
+        public T Get(Guid externalId)
+        {
+            return null;
+        }
+
+        public void Add(T data)
+        {
+
+        }
+
+        public void Update(T data)
+        {
+
+        }
+
+
+        public void Delete(Guid externalId)
+        {
+
+        }
+    }
+}
