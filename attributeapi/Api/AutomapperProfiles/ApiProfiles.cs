a4f4cd23d06ffc5bfb5e038a2344e7a1156bb0e9
mperdeck@gmail.com <mperdeck@gmail.com>
2019-11-03 18:18:55 +1100

    add automapper configuration

---
+++ attributeapi/Api/AutomapperProfiles/ApiProfiles.cs
@@ -0,0 +1,17 @@
+﻿using Api.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.AutomapperProfiles
+{
+    public class ApiProfiles : Profile
+    {
+        public ApiProfiles()
+        {
+            CreateMap<SimpleAttrViewModel, Data.SimpleAttr>().ReverseMap();
+        }
+    }
+}
--- attributeapi/Api/Startup.cs
+++ attributeapi/Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,12 +14,12 @@ namespace Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
 
-        public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,6 +31,15 @@ namespace Api
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            // New
+
+            // Find all automapper profiles in this assembly and configure them.
+            // Requires package reference to AutoMapper.Extensions.Microsoft.DependencyInjection
+            // See https://medium.com/ps-its-huuti/how-to-get-started-with-automapper-and-asp-net-core-2-ecac60ef523f
+            services.AddAutoMapper();
+
+            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
